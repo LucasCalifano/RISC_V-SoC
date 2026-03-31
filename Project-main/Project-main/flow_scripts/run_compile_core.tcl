b4f6152bbf0b1bca989cb988935c64dc94dddea5
HarrisonMills <160181455+HarrisonMills@users.noreply.github.com>
2026-03-31 15:34:30 -0400

    Update run_compile_core.tcl

--- Project-main/Project-main/flow_scripts/run_compile_core.tcl
+++ Project-main/Project-main/flow_scripts/run_compile_core.tcl
@@ -1,40 +1,53 @@
+
 ####### Design Setup ########
-set module riscv_core 
+set module riscv_core
 set outDir "./results"
+
+# Create directories if they don't exist
+file mkdir $outDir
+file mkdir ./WORK
 define_design_lib work -path ./WORK
 
-####### Synopsys 14nm #######
-set target_library "/home/net/local/SAED14nm_EDK_08_2024/SAED14nm_EDK_STD_RVT/liberty/nldm/base/saed14rvt_base_ss0p72v125c.db"
-set link_library "/home/net/local/SAED14nm_EDK_08_2024/SAED14nm_EDK_STD_RVT/liberty/nldm/base/saed14rvt_base_ss0p72v125c.db" 
+####### SkyWater 130nm Setup #######
+# Absolute path to your .db files
+set lib_path "/home/net/ha934819/libraries/libraries/sky130_fd_sc_hd/db"
+
+# Setting the Typical (TT) corner as the target
+set target_library "$lib_path/sky130_fd_sc_hd__tt_025C_1v80.db"
+
+# link_library: includes the design (*), the cells, and the Synopsys standard parts
+set link_library "* $target_library dw_foundation.sldb"
+
+# Update search_path so DC knows where to find files
+set_app_var search_path ". $lib_path ../riscv_soc_master/core/core/rv32imsu/ $search_path"
 
 ####### Load Design and Elaborate #######
-#analyze -format verilog -autoread ../core/core/rv32imsu/ 
-analyze -format verilog { 
-../core/rv32imsu/riscv_decode.v
-../core/rv32imsu/riscv_lsu.v
-../core/rv32imsu/riscv_regfile.v
-../core/rv32imsu/riscv_defs.v
-../core/rv32imsu/riscv_muldiv.v
-../core/rv32imsu/riscv_alu.v
-../core/rv32imsu/riscv_csr.v
-../core/rv32imsu/riscv_mmu_arb.v
-../core/rv32imsu/riscv_core.v
-../core/rv32imsu/riscv_mmu.v
-../core/rv32imsu/riscv_fetch.v
-../core/rv32imsu/riscv_exec.v
+# Paths are relative to your current location in /flow_scripts/
+# riscv_defs.v is listed first to ensure macros/parameters are defined
+analyze -format verilog {
+    ../riscv_soc_master/core/core/rv32imsu/riscv_defs.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_decode.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_lsu.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_regfile.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_muldiv.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_alu.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_csr.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_mmu_arb.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_core.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_mmu.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_fetch.v
+    ../riscv_soc_master/core/core/rv32imsu/riscv_exec.v
 }
+
 set top $module
 elaborate -lib work $top
 current_design $top
 link
 
-####### Design Flattening #######
-#set_flatten true
-#uniquify -force
-#ungroup -flatten -all
-
 ####### Design Specification ####
-source ./ss_0p9v_125c.tcl
+# Ensure this file exists in /flow_scripts/
+# Note: You may need to adjust clock periods here for 130nm vs 14nm
+source ./ss_1p6v_100c.tcl
 current_design $top
 
 ####### Compile and Optimize ####
